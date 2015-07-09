6395b6ea6af5771b72f8defdbcd5888f5f00afff
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-07-09 21:56:30 +0900

    mokey patch.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,15 @@
-require "docker"
-require "serverspec"
+require 'docker'
+require 'serverspec'
+
+if ENV['CIRCLECI']
+  class Docker::Container
+    def remove(options={}); end
+    alias_method :delete, :remove
+  end
+end
 
 set :backend, :docker
-set :docker_url, ENV["DOCKER_HOST"]
-set :docker_image, "rudolphm/clack-test"
+set :docker_url, ENV['DOCKER_HOST']
+set :docker_image, 'rudolphm/clack-test'
 
 Excon.defaults[:ssl_verify_peer] = false
