3f95eaab62749744f17581699a8fb4c1ddbb4e8a
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-07-10 00:31:51 +0900

    add tests of ros command.

--- spec/roswell_spec.rb
+++ spec/roswell_spec.rb
@@ -3,3 +3,7 @@ require 'spec_helper'
 describe file('/usr/local/bin/ros') do
     it { should be_executable }
 end
+
+describe command('ros list installed sbcl-bin') do
+  its (:stdout) { should match /sbcl-bin/ }
+end
